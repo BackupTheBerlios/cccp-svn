9dabe868add8ecde91fbd1b9c50b24f909e34ada
aferber <aferber@2737c7e6-75de-0310-a14e-ca99b888edd9>
2004-07-14 17:38:46 +0000

    add documentation for debug macros

--- trunk/cccp/include/cccp/debug.h
+++ trunk/cccp/include/cccp/debug.h
@@ -9,10 +9,25 @@
 #ifndef CCCP_DEBUG_H
 #define CCCP_DEBUG_H 1
 
+/*! \file
+ * A collection of various macros that help in debugging.
+ * The macros in this file can only be used inside non-static methods of
+ * classes derived from CCCP::Object, since they depend on methods
+ * inherited from there to do the grunt work.
+ *
+ * \todo Replace/extend with a more flexible logging framework.
+ */
+
 #if HAVE_AUTOSPRINTF
 
 #include <autosprintf.h>
 
+/*! \defgroup assertions Assertion macros
+ */
+
+/*! @{ */
+
+//! Log a bug with file and line number, and abort().
 #define BUG(args) do_BUG(__FILE__, __LINE__, gnu::autosprintf args)
 
 #else // !HAVE_AUTOSPRINTF
@@ -21,11 +36,21 @@
 
 #endif // !HAVE_AUTOSPRINTF
 
+//! Log bug and abort if \a expr evaluates true.
+/*!
+ * The same as
+ * \code
+ * if (expr)
+ *     BUG("expr")
+ * \endcode
+ */
 #define BUG_ON(expr) do { \
         if (expr) \
             do_BUG(__FILE__, __LINE__, #expr); \
     } while (0)
 
 #endif /* !defined(CCCP_DEBUG_H) */
+
+/*! @} */
 
 // vim:ft=cpp
